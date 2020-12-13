1a9106fde4358695d166b8b3e447fe80d7e0d69e
João Pedro <joaopedronorbertodasilva@gmail.com>
2020-12-13 18:14:13 -0300

    joao 4a
    login habilitado para landingpage

--- index.js
+++ index.js
@@ -14,21 +14,25 @@ app.get('/', (req, res)=>{
     res.render('index');
 });
 
-
 app.get('/login',(req, res) =>{
      res.render('login');
 });
 
-
-<<<<<<< HEAD
 app.get('/places', (req, res)=>{
     res.render('places/places');
 });
-=======
+
+app.get('/login',(req,res) =>{
+     res.render('login')
+})
+
+app.get('/landingpage',(req,res) =>{
+    res.render('landingpage')
+})
+
 app.get('/autenticacao', (req,res) =>{
     res.render('autenticacao')
 })
->>>>>>> 2bb323e9d123e2b3c468f3b0a4d9b2e21e320317
 
 app.listen(3000, ()=>{
     console.log('Servidor Rodando!');
