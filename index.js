a6609b3a245d7fbd8ea886d7396ab5b6d148772a
VictorRevers <victorrkasnowski@gmail.com>
2020-12-13 13:06:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor Rodando!');
+})
