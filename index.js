3fcd3ffff94ed1bd9cab109217ea1ed906d856bf
VictorRevers <victorrkasnowski@gmail.com>
2020-12-13 19:19:40 -0300

    victor3A

--- index.js
+++ index.js
@@ -20,15 +20,19 @@ app.get('/login',(req, res) =>{
 });
 
 
-<<<<<<< HEAD
 app.get('/places', (req, res)=>{
     res.render('places/places');
 });
-=======
-app.get('/autenticacao', (req,res) =>{
-    res.render('autenticacao')
-})
->>>>>>> 2bb323e9d123e2b3c468f3b0a4d9b2e21e320317
+
+
+
+
+
+
+
+app.get('/coletor/receber', (req, res)=>{
+    res.render('colector/receive');
+});
 
 app.listen(3000, ()=>{
     console.log('Servidor Rodando!');
